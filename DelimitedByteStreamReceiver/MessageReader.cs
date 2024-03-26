2a68d1c01f488ee92b94bb9826def628ee32fb8a
oid85 <oid85@mail.ru>
2024-03-26 17:32:35 +0700

    TASK-01_Add method ReadMessage and edit Program.cs

---
+++ DelimitedByteStreamReceiver/MessageReader.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// Класс, с методами для работы с байтовым потоком
+/// </summary>
+public class MessageReader
+{
+    /// <summary>
+    /// Чтение потока, пока не встретится разделитель
+    /// </summary>
+    /// <param name="delimiter">Байт разделитель</param>
+    public byte[] ReadMessage(Stream stream, byte delimiter)
+    {
+        try
+        {
+            using var messageBuffer = new MemoryStream();
+            int currentByte;
+
+            // Пока не встретится конец потока
+            while ((currentByte = stream.ReadByte()) != -1)
+            {
+                // Если встретился разделитель, то прерываемся
+                if (currentByte == delimiter)
+                    break;
+
+                messageBuffer.WriteByte((byte)currentByte);
+            }
+
+            var result = messageBuffer.ToArray();
+
+            return result;
+        }
+
+        catch (Exception exception)
+        {
+            Console.WriteLine($"ReadMessage error: {exception.Message}");
+            
+            return Array.Empty<byte>();
+        }        
+    }
+}
---
+++ DelimitedByteStreamReceiver/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Создадим случайный массив длины больше, чем 255 (чтобы как минимум две части у нас были)
+        byte[] bytes = GenerateRandomByteArray(500);
+        const byte delimiter = 127;
+        int partNumber = 0;
+
+        // Читаем входной поток
+        using var stream = new MemoryStream(bytes);
+
+        var reader = new MessageReader();
+
+        while (true)
+        {
+            byte[] message = reader.ReadMessage(stream, delimiter);
+
+            if (message.Length == 0)
+                break;
+
+            partNumber++;
+
+            Console.WriteLine($"Received message (part number {partNumber}): {BitConverter.ToString(message)}");
+        }
+    }
+
+    /// <summary>
+    /// Случайный массив байт длины n
+    /// </summary>
+    static byte[] GenerateRandomByteArray(int n)
+    {
+        var rand = new Random();
+        var byteArray = new byte[n];
+
+        rand.NextBytes(byteArray);
+
+        return byteArray;
+    }
+}
